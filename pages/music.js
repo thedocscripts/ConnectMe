891cf5698e45836a63664c24219a9f9c838b759e
Thedoc <thedocscripts@gmail.com>
2023-02-15 12:01:56 +0100

    a

---
+++ pages/music.js
@@ -0,0 +1,19 @@
+import { useUser } from '@auth0/nextjs-auth0/client';
+import Link from 'next/link'
+
+export default function About() {
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
+  if (user) {
+    return (
+      <div>
+        Welcome {user.name}! <Link href="/api/auth/logout">Logout</Link>
+      </div>
+    );
+  }
+
+  return <Link href="/api/auth/login">Login</Link>;
+}
