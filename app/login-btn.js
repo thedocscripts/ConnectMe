0e2087e0bba01b4223e9fc93ac93da7c9d4c45d4
Thedoc <thedocscripts@gmail.com>
2023-02-16 22:46:37 +0100

    tremendo fix

--- app/login-btn.js
+++ app/login-btn.js
@@ -1,21 +1,25 @@
 "use client"
 import { useSession, signIn, signOut } from "next-auth/react"
+import { useRouter } from "next/navigation"
 import UserInformation from './user-information'
+
 export default function Component() {
   const { data: session } = useSession()
+  const router = useRouter();
+
   if (session) {
     return (
       <>
         Signed in as {session.user.email} <br />
         <UserInformation data={session.user} />
-        <button onClick={() => signOut()}>Sign out</button>
+        <button onClick={() => router.push("/api/auth/signout")}>Sign out</button>
       </>
     )
   }
   return (
     <>
       Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
+      <button onClick={() => router.push("/api/auth/signin")}>Sign in</button>
     </>
   )
 }
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,7 +3,6 @@ import GithubProvider from "next-auth/providers/github"
 import DiscordProvider from "next-auth/providers/discord"
 import GoogleProvider from "next-auth/providers/google"
 import { FirestoreAdapter } from "@next-auth/firebase-adapter"
-import CredentialsProvider from "next-auth/providers/credentials";
 import { initializeApp } from 'firebase/app';
 import * as admin from 'firebase-admin'
 
@@ -12,9 +11,8 @@ import * as admin from 'firebase-admin'
 
 
 export const authOptions = {
-    
   // Configure one or more authentication providers
-
+ 
     // callbacks: {
     //     session: async (session, token) => {
     //       session.user.id = token.id;
@@ -23,39 +21,7 @@ export const authOptions = {
     //     },
     //   },
   providers: [
-    // CredentialsProvider({
-    //     // The name to display on the sign in form (e.g. "Sign in with...")
-    //     name: "Usuario / Contraseña",
-    //     // `credentials` is used to generate a form on the sign in page.
-    //     // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-    //     // e.g. domain, username, password, 2FA token, etc.
-    //     // You can pass any HTML attribute to the <input> tag through the object.
-    //     credentials: {
-    //       username: { label: "Username", type: "text", placeholder: "jsmith" },
-    //       password: { label: "Password", type: "password" }
-    //     },
-    //     async authorize(credentials, req) {
-    //         // You need to provide your own logic here that takes the credentials
-    //         // submitted and returns either a object representing a user or value
-    //         // that is false/null if the credentials are invalid.
-    //         // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
-    //         // You can also use the `req` object to obtain additional parameters
-    //         // (i.e., the request IP address)
-    //         const res = await fetch("/your/endpoint", {
-    //           method: 'POST',
-    //           body: JSON.stringify(credentials),
-    //           headers: { "Content-Type": "application/json" }
-    //         })
-    //         const user = await res.json()
-      
-    //         // If no error and we have user data, return it
-    //         if (res.ok && user) {
-    //           return user
-    //         }
-    //         // Return null if user data could not be retrieved
-    //         return null
-    //       }
-    //   }),
+   
     
     GithubProvider({
       clientId: process.env.GITHUB_ID,
@@ -75,7 +41,7 @@ export const authOptions = {
    credential: admin.credential.cert({
     projectId: process.env.PROYECT_ID ? process.env.PROYECT_ID.replace(/\\n/gm, "\n") : undefined,
      clientEmail: process.env.CLIENT_EMAIL ? process.env.CLIENT_EMAIL.replace(/\\n/gm, "\n") : undefined,
-     privateKey: process.env.PRIVATE_KEY ? process.env.PRIVATE_KEY  .replace(/\\n/gm, "\n") : undefined,
+     privateKey: process.env.PRIVATE_KEY ? process.env.PRIVATE_KEY.replace(/\\n/gm, "\n") : undefined,
    })
   }),
 
