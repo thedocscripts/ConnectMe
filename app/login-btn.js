8709b832d43e9b16a6f3decb63d42080ad4d4478
Thedoc <thedocscripts@gmail.com>
2023-02-17 00:01:01 +0100

    Update login-btn.js

--- app/login-btn.js
+++ app/login-btn.js
@@ -1,19 +1,53 @@
 "use client"
 import { useSession, signIn, signOut } from "next-auth/react"
 import { useRouter } from "next/navigation"
+import { toast, ToastContainer } from "react-toastify"
 import UserInformation from './user-information'
+import 'react-toastify/dist/ReactToastify.css';
+import { useRef } from "react"
+
+
+
+
+const customId = "custom-id-yes";
+
+
+  
 
 export default function Component() {
-  const { data: session } = useSession()
+  const { data: session, status } = useSession()
   const router = useRouter();
+  const toastId = useRef(null);
 
+
+  const notify = (name) => {
+    if(! toast.isActive(toastId.current)) {
+        toastId.current = toast("Bienvenido: " + name);
+      }
+    
+  }
+
+  if(status == "loading"){
+    
+    
+    return(
+        <div>
+            Loading...
+        
+        </div>
+    )
+  }
   if (session) {
+    console.log(status)
+    notify(session.user.name)
     return (
-      <>
+      <div>
+        
         Signed in as {session.user.email} <br />
         <UserInformation data={session.user} />
         <button onClick={() => router.push("/api/auth/signout")}>Sign out</button>
-      </>
+        <ToastContainer />
+      </div>
     )
   }
   return (
