7ef8aacd61e43e7a86d2c0f4e52b007b266a87f5
Thedoc <thedocscripts@gmail.com>
2023-03-10 10:44:00 +0100

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -341,15 +341,15 @@ const Team = ()=> <section className="pt-20 pb-48">
     </div>
     <div className="w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4">
       <div className="px-6">
-        {/* <img
+        <Image
           alt="..."
           src={require('../assets/img/team-3-800x800.jpg')}
           className="shadow-lg rounded-full max-w-full mx-auto"
           style={{ maxWidth: "120px" }}
-        /> */}
+        />
         <div className="pt-6 text-center">
           <h5 className="text-xl font-bold">
-            Alexa Smith
+            Javier Tortosa
               </h5>
           <p className="mt-1 text-sm text-gray-500 uppercase font-semibold">
             UI/UX Designer
@@ -378,48 +378,7 @@ const Team = ()=> <section className="pt-20 pb-48">
       </div>
     </div>
     <div className="w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4">
-      <div className="px-6">
-        {/* <img
-          alt="..."
-          src={require('../assets/img/team-4-470x470.png')}
-          className="shadow-lg rounded-full max-w-full mx-auto"
-          style={{ maxWidth: "120px" }}
-        /> */}
-        <div className="pt-6 text-center">
-          <h5 className="text-xl font-bold">
-            Jenna Kardi
-              </h5>
-          <p className="mt-1 text-sm text-gray-500 uppercase font-semibold">
-            Founder and CEO
-              </p>
-          <div className="mt-6">
-            <button
-              className="bg-pink-500 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
-              type="button"
-            >
-              <i className="fab fa-dribbble"></i>
-            </button>
-            <button
-              className="bg-red-600 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
-              type="button"
-            >
-              <i className="fab fa-google"></i>
-            </button>
-            <button
-              className="bg-blue-400 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
-              type="button"
-            >
-              <i className="fab fa-twitter"></i>
-            </button>
-            <button
-              className="bg-gray-800 text-white w-8 h-8 rounded-full outline-none focus:outline-none mr-1 mb-1"
-              type="button"
-            >
-              <i className="fab fa-instagram"></i>
-            </button>
-          </div>
-        </div>
-      </div>
+      
     </div>
   </div>
 </div>
