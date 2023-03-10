fb059236d6bb79b182c2de578ddee28ffc8200a1
Thedoc <thedocscripts@gmail.com>
2023-03-10 10:43:11 +0100

    as

--- app/api/route.js
+++
@@ -1,5 +0,0 @@
-export const payload = { name: 'John Doe' };
-
-export default function handler(req, res) {
-  res.status(200).json(payload)
-}
--- firebase.js
+++ firebase.js
@@ -1,5 +1,5 @@
 import {initializeApp, applicationDefault, cert, getApps} from 'firebase/app'
-import { getFirestore, Timestamp, FieldValue,  } from 'firebase/firestore';
+import { getFirestore, Timestamp, FieldValue, getDocs, collection,  } from 'firebase/firestore';
 
 var serviceAccount = require("@/public/connectme.json");
 
@@ -21,7 +21,30 @@ const db = getFirestore();
        
 //   }
 
+async function  getallusers(){
+  const querySnapshot = await getDocs(collection(db, "users"));
+  var users = []
+        querySnapshot.forEach((doc) => {
+          // doc.data() is never undefined for query doc snapshots
+          
+          users.push(doc.data())
+        });
+        return users
+}
 
 
+async function  getuser(user){
+  const querySnapshot = await getDocs(collection(db, "users"));
+  
+        querySnapshot.forEach((doc) => {
+          // doc.data() is never undefined for query doc snapshots
+          if(user == doc.data().email){
+            return doc.data()
+          }
+          
+        });
+        return null
+}
 
-export { db }
+
+export { db, getallusers, getuser }
